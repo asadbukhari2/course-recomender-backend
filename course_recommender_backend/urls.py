593d28e5a8624c0ec743df603a9e9680c34a3631
Asad Bukhari <57019121+asdbukhari@users.noreply.github.com>
2022-12-14 22:05:43 +0500

    update

--- course_recommender_backend/urls.py
+++ course_recommender_backend/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path(r'api/', include('course_recommender.urls')),
+    path('api/', include('course_recommender.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
